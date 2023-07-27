5f17f2dc61195c333065efab8be877dbc66534bf
stordd13 <stordeur.bru@gmail.com>
2023-07-27 09:43:11 +0200

    dvc added

--- src/cnnClassifier/pipeline/stage_01_data_ingestion.py
+++ src/cnnClassifier/pipeline/stage_01_data_ingestion.py
@@ -19,7 +19,7 @@ class DataIngestionTrainingPipeline:
 if __name__ == "__main__":
     try:
         logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
-        obj = DataIngestionTrainingpipeline()
+        obj = DataIngestionTrainingPipeline()
         obj.main()
         logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nx==========x")
     except Exception as e:
--- src/cnnClassifier/pipeline/stage_04_evaluation.py
+++ src/cnnClassifier/pipeline/stage_04_evaluation.py
@@ -24,4 +24,4 @@ if __name__ == "__main__":
         logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nx==========x")
     except Exception as e:
         logger.exception(e)
-    raise e
+        raise e
